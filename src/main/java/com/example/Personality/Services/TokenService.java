f7e6c1c5ab5743b0e167f3002b26be370d018c9a
tfuok <144696566+tfuok@users.noreply.github.com>
2025-06-27 03:21:09 +0700

    Modified application.properties

--- src/main/java/com/example/Personality/Services/TokenService.java
+++ src/main/java/com/example/Personality/Services/TokenService.java
@@ -7,6 +7,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
@@ -17,18 +18,18 @@ public class TokenService {
     @Autowired
     UserRepository userRepository;
 
-    public final String SECRET_KEY = "4bb6d1dfbafb64a681139d1586b6f1160d18159afd57c8c79136d7490630407p";
+    @Value("${jwt.secret}")
+    private String secretKey;
 
     private SecretKey getSignInKey() {
-        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
         return Keys.hmacShaKeyFor(keyBytes);
     }
 
-    //tạo ra token
     public String generateToken(User account) {
         String token = Jwts.builder()
                 .subject(account.getId() + "")
-                .issuedAt(new Date(System.currentTimeMillis())) // vd: tạo lúc 10h
+                .issuedAt(new Date(System.currentTimeMillis()))
                 .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
                 .signWith(getSignInKey())
                 .compact();
