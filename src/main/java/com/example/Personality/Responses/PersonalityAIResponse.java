e9cd9aee3d9d702a035f0736b4e74aac67954875
tfuok <144696566+tfuok@users.noreply.github.com>
2025-06-27 16:43:23 +0700

    Update .gitignore and remove sensitive files

---
+++ src/main/java/com/example/Personality/Responses/PersonalityAIResponse.java
@@ -0,0 +1,4 @@
+package com.example.Personality.Responses;
+
+public class PersonalityAIResponse {
+}
---
+++ src/main/java/com/example/Personality/Services/OpenRouterService.java
@@ -0,0 +1,4 @@
+package com.example.Personality.Services;
+
+public class OpenRouterService {
+}
